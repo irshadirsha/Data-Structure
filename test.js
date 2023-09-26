79205859fa56442a2fcfbebf3b465613b34d7da5
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-09-26 22:03:06 +0530

    binarysearch

---
+++ test.js
@@ -0,0 +1,20 @@
+function binarysearch(arr,target){
+    let left=0;
+    let right=arr.length-1;
+
+    while(left<=right){
+        const mid=Math.floor((left+right)/2)
+
+        if(arr[mid]==target){
+            return mid
+        }else if(arr[mid]<target){
+            left=mid+1
+
+        }else{
+            right=mid-1
+        }
+    }
+  return -1
+}
+let arr=[1,3,7,23,67,99]
+console.log(binarysearch(arr,-23))
