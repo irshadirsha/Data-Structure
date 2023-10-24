d06b18075a344cc85649250f6552f50080e2f78d
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-10-24 21:23:00 +0530

    string reverse

---
+++ stringRev.js
@@ -0,0 +1,12 @@
+function reverseString(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+    }
+    return reversed;
+  }
+  
+  const originalString = "Hello, World!";
+  const reversedString = reverseString(originalString);
+  console.log(reversedString); 
+  
