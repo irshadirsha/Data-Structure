a4cf190f8906bc831f16d62c3f2ff4ed20040956
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-11-05 21:26:20 +0530

    updation

--- removeduplicate.js
+++ removeduplicate.js
@@ -1,21 +1,15 @@
-const arr = [3, 4, 5, 4, 5, 7, 8];
+const arr = [3,8, 4, 5, 4, 5, 7,8, 8];
 const uniqueArr = [];
-
 for (let i = 0; i < arr.length; i++) {
   let isDuplicate = false;
-
-  // Check if the current element is a duplicate
   for (let j = 0; j < uniqueArr.length; j++) {
     if (arr[i] === uniqueArr[j]) {
-      isDuplicate = true;
+      isDuplicate = true;                  DUPLICATE
       break;
     }
   }
-
-  // If it's not a duplicate, add it to the uniqueArr
   if (!isDuplicate) {
     uniqueArr.push(arr[i]);
   }
 }
-
 console.log(uniqueArr); // Output: [3, 4, 5, 7, 8]
--- test.js
+++ test.js
@@ -1,23 +1,24 @@
-// function binarysearch(arr,target){
-//     let left=0;
-//     let right=arr.length-1;
+function binarysearch(arr,target){
+    let left=0;
+    let right=arr.length-1;
 
-//     while(left<=right){
-//         const mid=Math.floor((left+right)/2)
+    while(left<=right){
+        const mid=Math.floor((left+right)/2)
 
-//         if(arr[mid]==target){
-//             return mid
-//         }else if(arr[mid]<target){
-//             left=mid+1
+        if(arr[mid]==target){
+            return mid
+        }else if(arr[mid]<target){
+            left=mid+1
 
-//         }else{
-//             right=mid-1
-//         }
-//     }
-//   return -1
-// }
-// let arr=[1,3,7,23,67,99]
-// console.log(binarysearch(arr,-23))
+        }else{
+            right=mid-1
+        }
+    }
+  return -1
+}
+
+let arr=[1,3,7,23,67,99]
+console.log(binarysearch(arr,-23))
 let arr1=[2,5,9,11]
 let arr2=[1,5,7,10]
 function mergesort(arr1,arr2){
