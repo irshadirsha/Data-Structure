09f3afccba7cac861f9d7f04e626b83d4a47c440
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-12-02 15:00:40 +0530

    correction

--- plusOne.js
+++ plusOne.js
@@ -1,9 +1,17 @@
 var plusOne = function(digits) {
+    console.log(digits)
+    let arr=[]
     let number=digits.join('')
-    let numbers=parseInt(number)
-    value= numbers+1
-    return value
+    console.log(number);
+    let numbers=BigInt(number)
+    console.log(numbers);
+     arr.push(numbers+1n)
+    const num = arr[0].toString().split('').map(Number)
+     console.log(arr);
+     console.log(num);
+     return num
+
 };
 
-digit = [4,3,2,1]
+digit = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
 console.log(plusOne(digit))
