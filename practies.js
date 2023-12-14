eaee54a68ae4de3b180c78f74f3d42556ff69ae8
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-12-14 21:40:10 +0530

    practies

--- practies.js
+++ practies.js
@@ -25,3 +25,21 @@ const myPromise = new Promise((resolve, reject) => {
       console.error('Error:', error);
     });
   
+
+    var plusOne = function(digits) {
+      console.log(digits)
+      let arr=[]
+      let number=digits.join('')
+      console.log(number);
+      let numbers=BigInt(number)
+      console.log(numbers);
+       arr.push(numbers+1n)
+      const num = arr[0].toString().split('').map(Number)
+       console.log(arr);
+       console.log(num);
+       return num
+  
+  };
+  
+  digit = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
+  console.log(plusOne(digit))
