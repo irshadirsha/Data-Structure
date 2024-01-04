2b97ff449a9ef9aaed5f78cabcfda621a36d85cf
Mahammad Irshad ap <irshadalike10@gmail.com>
2024-01-04 15:06:49 +0530

    soc

--- practies.js
+++ practies.js
@@ -26,3 +26,43 @@ function isPrime(number){
 app.listen(port,()=>{
   console.log("server running")
 })
+
+import { useState,useEffect } from 'react'
+import './App.css'
+import io from "socket.io-client"
+const socket = io.connect("http://localhost:4000")
+function App() {
+   const [message,setMessage]=useState("")
+   const [recmessages,setRecMessages]=useState("")
+   console.log(message)
+  function handleSend(){
+    console.log("button clicked")
+    socket.emit("send_message",{message:message})
+    setMessage('')
+
+  }
+  useEffect(()=>{
+   socket.on("recived_message",(data)=>{
+    console.log("in front end ",data)
+    console.log("in front end ",data.message)
+    console.log("in front end ",data.message.message)
+   setRecMessages(data.message.message )
+  // setRecMessages(JSON.stringify(data.message));
+   })
+  },[socket])
+  return (
+    <>
+      <div>
+    <input placeholder='Message...'
+    value={message}
+    onChange={(e)=>{setMessage(e.target.value)}}
+    />
+    <button onClick={handleSend}>send message</button>
+     <h5>recived messages=={recmessages}</h5>
+   
+      </div>
+    </>
+  )
+}
+
+export default App
