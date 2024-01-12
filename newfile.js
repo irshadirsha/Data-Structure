fa83186c01ad65c6588f1ed6ac174e19cca34401
Mahammad Irshad ap <irshadalike10@gmail.com>
2024-01-12 21:45:29 +0530

    pr

--- newfile.js
+++ newfile.js
@@ -1,21 +1,30 @@
-const data = [
-    { id: 1, name: 'test' },
-    { id: 2, name: 'test2' },
-    { id: 3, name: 'test3' },
-    { id: 1, add: 'jaipur' },
-    { id: 2, add: 'kota' },
-    { id: 3, add: 'jaipur' },
-    { id: 1, phone: '123456' }, 
-    { id: 2, phone: '234234234' }
-];
+// const data = [
+//     { id: 1, name: 'test' },
+//     { id: 2, name: 'test2' },
+//     { id: 3, name: 'test3' },
+//     { id: 1, add: 'jaipur' },
+//     { id: 2, add: 'kota' },
+//     { id: 3, add: 'jaipur' },
+//     { id: 1, phone: '123456' }, 
+//     { id: 2, phone: '234234234' }
+// ];
 
-const result = {};
+// const result = {};
 
-data.forEach(item => {
-    if (!result[item.id]) {
-        result[item.id] = [];
-    }
-    result[item.id].push(item);
-});
+// data.forEach(item => {
+//     if (!result[item.id]) {
+//         result[item.id] = [];
+//     }
+//     result[item.id].push(item);
+// });
 
-console.log(result);
+// console.log(result);
+
+let a = 6;
+let b = 5;
+
+// Swapping values without a third variable
+[a, b] = [b, a];
+
+console.log("a:", a); // Output: [6]
+console.log("b:", b); // Output: [4]
