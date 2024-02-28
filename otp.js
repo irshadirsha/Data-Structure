700af9b41f058423466002731a3e76171aa59731
Mahammad Irshad ap <irshadalike10@gmail.com>
2024-02-28 12:32:08 +0530

    otp

---
+++ otp.js
@@ -0,0 +1,13 @@
+
+
+// function generateOtp(){
+//     let otplength=6
+//     let otp=""
+//     for(let i=0;i<otplength;i++){
+//         otp+=Math.floor(Math.random()*10)
+//     }
+//     return otp
+// }
+// let otp=generateOtp()
+// console.log("otp",otp)
+
