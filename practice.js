5085260a167c13cc5d4c05c92c827d2e20383043
Mahammad Irshad ap <irshadalike10@gmail.com>
2024-04-08 14:32:01 +0530

    rev

--- practice.js
+++ practice.js
@@ -192,3 +192,31 @@
 
 // let words = ["mass", "as", "hero", "superhero"];
 // console.log(stringMatching(words)); // Output: ["as", "hero"]
+
+
+// var commonChars = function(words) {
+//    console.log("log words",words)
+//    let newarr=[]
+//    let str=words[0]
+//    for(let i=0;i<str.length;i++){
+//       console.log("first loop i--",str[i]);
+//       let flag=0
+//       for(let j=1;j<words.length;j++){
+//       console.log("second loop i--",words[j]);
+
+//          if(!words[j].includes(str[i])){
+//             flag=1
+//          }else{
+//             words[j]=words[j].replace(str[i],"")
+//          }
+//       }
+//       console.log(flag);
+//       if(flag==0){
+//       newarr.push(str[i])
+//       }
+//    }
+//    console.log(newarr);
+// };
+// words = ["cool","lock","cook"]
+// commonChars(words)
+// // Output: ["e","l","l"]
